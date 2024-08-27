314cdf76c34e2f6118432cf51a639650ca5838f4
Yonatan Diga <digayona@post.bgu.ac.il>
2024-08-27 15:41:35 +0300

    works with serious eats, cv2 import isn't working so working with pictures  currently not working

--- Business/convertToText.py
+++ Business/convertToText.py
@@ -1,5 +1,5 @@
 import pytesseract
-import cv2
+#import cv2
 
 
 
@@ -8,15 +8,15 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\\Users\\יונתן\\AppData\\Local\
 
 
 def readPicture(nameOfPicture):
-
-    img = cv2.imread(picturesPath + nameOfPicture)
-
-    text = pytesseract.image_to_string(img)
-    splitted = text.split('\n')
-    removeSpace = [x for x in splitted if x != ""]
-
-    result = removeSpace
-    return result
+    pass
+    # img = cv2.imread(picturesPath + nameOfPicture)
+    #
+    # text = pytesseract.image_to_string(img)
+    # splitted = text.split('\n')
+    # removeSpace = [x for x in splitted if x != ""]
+    #
+    # result = removeSpace
+    # return result
 
 
 def readPictureFromWeb(img):
--- Presenation/WebMainPage.py
+++ Presenation/WebMainPage.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter import filedialog
 import numpy as np
-import cv2
+#import cv2
 from Business.Conversions import convertToGrams
 from Business.convertToText import readPictureFromWeb
 from Business.seleniumConvertToText import getIngredientsFromWebScraping
@@ -15,6 +15,13 @@ title = Label(app, text='Recipe Converter', font=('Times', 24), fg='#068481')
 title.pack()
 
 
+def processURL(url):
+    if url is None:
+        return
+    original, newIngredients = startFuncFromWebScraping(url)
+    printList(newIngredients)
+
+
 def printList(listToPrint):
     root = Tk()
     root.geometry("1024x640")
@@ -24,9 +31,11 @@ def printList(listToPrint):
     txt_output.config(font=('Helvatical bold', 20))
     txt_output.pack(pady=30)
 
+
     for item in listToPrint:
         txt_output.insert(END, item + "\n")
     root.mainloop()
+
 
 def openAndPut():
     path = filedialog.askopenfilename()
@@ -37,21 +46,37 @@ def openAndPut():
 
 
 
-image_area = Canvas(app, width=490, height=500, bg='#C8C8C8')
-image_area.pack(pady=(10, 0))
+# image_area = Canvas(app, width=490, height=500, bg='#C8C8C8')
+# image_area.pack(pady=(10, 0))
 
 open_image = Button(app, width=20, text='OPEN IMAGE', font=('Helvatical bold', 20), command=openAndPut)
 open_image.pack(pady=(10, 5))
 
 
+# Input field for the URL
+url_entry = Entry(app, width=50, font=('Helvetica', 16))
+url_entry.pack(pady=(10, 5))
+
+# Button to process the URL
+process_button = Button(app, width=20, text='PROCESS LINK', font=('Helvetica bold', 20),
+                        command=lambda: processURL(url_entry.get()))
+process_button.pack(pady=(10, 5))
+
+def startFuncFromWebScraping(url):
+    result = list()
+    ingredients = getIngredientsFromWebScraping(url)
+    for sentence in ingredients:
+        curr = convertToGrams(sentence)
+        result.append(curr)
+
+    return ingredients, result
 
 
 def startFuncForWeb(path):
     result = list()
-    img = cv2.imread(path)
+    #img = cv2.imread(path)
     #ingredients = readPictureFromWeb(img)  #reading the photo
-    link = "https://www.seriouseats.com/bravetarts-devils-food-cake"
-    ingredients = getIngredientsFromWebScraping(link)
+    ingredients = None
     for sentence in ingredients:
         curr = convertToGrams(sentence)
         result.append(curr)
